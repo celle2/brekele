a076e6c9e49ac7f754dc0c7d3f3f68da4ba2e0be
celle2 <108885778+celle2@users.noreply.github.com>
2022-07-07 22:07:13 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "brekele.py",
+    version = "0.1",
+    py_modules = ['brekele'],
+    url = "https://github.com/demotomohiro/remocolab",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
