be315e99b7214e14011ede8b860bcc10156aab85
celle2 <108885778+celle2@users.noreply.github.com>
2022-08-02 15:01:32 +0700

    Update brekele.py

--- brekele.py
+++ brekele.py
@@ -274,7 +274,7 @@ def _setupSSHDMain(public_key, tunnel, ngrok_region, check_gpu_available, mount_
       print("sa - South America (Sao Paulo)")
       print("jp - Japan (Tokyo)")
       print("in - India (Mumbai)")
-      ngrok_region = region = input(Ohio)
+      ngrok_region = us = input()
 
   return (True, _setupSSHDImpl(public_key, tunnel, ngrok_token, ngrok_region, mount_gdrive_to, mount_gdrive_from, is_VNC))
 
